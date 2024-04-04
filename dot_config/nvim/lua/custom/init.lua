c99946b8885809748dde7c33e4540705037c7a21
Joonas Rissanen <joonas@huuva.io>
2024-04-04 12:55:41 +0300

    Add autcmd to apply dot file changes

--- dot_config/nvim/lua/custom/init.lua
+++ dot_config/nvim/lua/custom/init.lua
@@ -1,7 +1,13 @@
--- local autocmd = vim.api.nvim_create_autocmd
-require "custom.configs.cmds"
+local autocmd = vim.api.nvim_create_autocmd
+require("custom.configs.cmds")
 -- Auto resize panes when resizing nvim window
 -- autocmd("VimResized", {
 --   pattern = "*",
 --   command = "tabdo wincmd =",
 -- })
+
+-- apply dotenv changes automatically on save
+autocmd("BufWritePost", {
+	pattern = vim.fn.expand("~") .. "/.local/share/chezmoi/*",
+	command = 'silent ! chezmoi apply --source-path "%"',
+})
