700e75d0cbfece5a7a032b2d8267fce02e6e065e
Joonas Rissanen <joonas@huuva.io>
2024-06-06 10:19:37 +0300

    Update visual multi config

--- dot_config/nvim/lua/custom/plugins/visual-multi.lua
+++ dot_config/nvim/lua/custom/plugins/visual-multi.lua
@@ -1,7 +1,13 @@
 --- @type NvPluginSpec
 local M = {
-  "mg979/vim-visual-multi",
-  event = { "BufReadPre", "BufNewFile" },
+	"mg979/vim-visual-multi",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		vim.g.VM_maps = {
+			["Add Cursor Up"] = "<C-k>",
+			["Add Cursor Down"] = "<C-j>",
+		}
+	end,
 }
 
 return M
