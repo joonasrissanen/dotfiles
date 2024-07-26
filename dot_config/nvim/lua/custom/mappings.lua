01e5fb4768af9c7d0311db79e718ad7966ea1c73
Joonas Rissanen <joonas@huuva.io>
2024-07-26 13:20:30 +0300

    Map 'jk' to esc

--- dot_config/nvim/lua/custom/mappings.lua
+++ dot_config/nvim/lua/custom/mappings.lua
@@ -2,29 +2,34 @@
 local M = {}
 
 M.disabled = {
-  n = {
-    -- disable default nvimtree toggle
-    -- as it collides with visual-multi
-    ["<C-n>"] = "",
-  },
+	n = {
+		-- disable default nvimtree toggle
+		-- as it collides with visual-multi
+		["<C-n>"] = "",
+	},
 }
 
 M.general = {
-  n = {
-    [";"] = { ":", "enter command mode", opts = { nowait = true } },
-    ["<C-t>"] = { "<cmd>NvimTreeToggle<cr>", "toggle nvimtree" },
+	n = {
+		[";"] = { ":", "enter command mode", opts = { nowait = true } },
+		["<C-t>"] = { "<cmd>NvimTreeToggle<cr>", "toggle nvimtree" },
+		["jk"] = { "<ESC>", "Escape key", opts = { nowait = true, silent = true } },
 
-    --  format with conform
-    ["<leader>fm"] = {
-      function()
-        require("conform").format()
-      end,
-      "formatting",
-    },
-  },
-  v = {
-    [">"] = { ">gv", "indent" },
-  },
+		--  format with conform
+		["<leader>fm"] = {
+			function()
+				require("conform").format()
+			end,
+			"formatting",
+		},
+	},
+	v = {
+		[">"] = { ">gv", "indent" },
+		["jk"] = { "<ESC>", "Escape key", opts = { nowait = true, silent = true } },
+	},
+	i = {
+		["jk"] = { "<ESC>", "Escape key", opts = { nowait = true, silent = true } },
+	},
 }
 
 -- more keybinds!
