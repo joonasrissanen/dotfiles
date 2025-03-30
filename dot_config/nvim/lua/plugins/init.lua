f85eccebc09c6741cca0da93f3ae16103aae91bd
Joonas Rissanen <joonas@huuva.io>
2025-03-30 18:35:29 +0300

    Add peek

--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -429,4 +429,14 @@ return {
     "nvim-tree/nvim-tree.lua",
     enabled = false,
   },
+  {
+    "toppair/peek.nvim",
+    event = { "VeryLazy" },
+    build = "deno task --quiet build:fast",
+    config = function()
+      require("peek").setup()
+      vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
+      vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
+    end,
+  },
 }
