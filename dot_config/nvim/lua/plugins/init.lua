536390ce2831299df03b4ca04554e9a943993e14
Joonas Rissanen <joonas@huuva.io>
2025-08-13 12:08:20 +0300

    Add nvim-surround

--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -342,6 +342,17 @@ return {
   },
 
   {
+    "kylechui/nvim-surround",
+    version = "^3.0.0", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup {
+        -- Configuration here, or leave empty to use defaults
+      }
+    end,
+  },
+
+  {
     "mg979/vim-visual-multi",
     event = "User FilePost",
     config = function()
