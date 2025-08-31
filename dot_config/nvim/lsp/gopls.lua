22939fcb959140634118de9534a173a6bb665149
Joonas Rissanen <joonas@huuva.io>
2025-08-31 11:20:51 +0300

    Cleanup cmds and autocmds

--- dot_config/nvim/lsp/gopls.lua
+++ dot_config/nvim/lsp/gopls.lua
@@ -1,4 +1,32 @@
 --- https://github.com/golang/tools/tree/master/gopls
+
+local function on_attach()
+  local autoimport = vim.api.nvim_create_augroup("joonasrissanen/lsp/gopls/autoimport", { clear = true })
+  vim.api.nvim_create_autocmd("BufWritePre", {
+    pattern = "*.go",
+    callback = function()
+      local params = vim.lsp.util.make_range_params()
+      params.context = { only = { "source.organizeImports" } }
+      -- buf_request_sync defaults to a 1000ms timeout. Depending on your
+      -- machine and codebase, you may want longer. Add an additional
+      -- argument after params if you find that you have to write the file
+      -- twice for changes to be saved.
+      -- E.g., vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 3000)
+      local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params)
+      for cid, res in pairs(result or {}) do
+        for _, r in pairs(res.result or {}) do
+          if r.edit then
+            local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
+            vim.lsp.util.apply_workspace_edit(r.edit, enc)
+          end
+        end
+      end
+      vim.lsp.buf.format { async = false }
+    end,
+    group = autoimport,
+  })
+end
+
 return {
   cmd = { "gopls" },
   filetypes = { "go", "gomod", "gosum", "gowork", "gotmpl" },
@@ -12,4 +40,8 @@ return {
       staticcheck = true,
     },
   },
+  on_attach = on_attach,
+  on_exit = function()
+    vim.api.nvim_clear_autocmds { group = "joonasrissanen/lsp/gopls/autoimport" }
+  end,
 }
--- dot_config/nvim/lua/autocmds.lua
+++ dot_config/nvim/lua/autocmds.lua
@@ -1,30 +1,33 @@
 local autocmd = vim.api.nvim_create_autocmd
 
+-- user event that loads after UIEnter + only if file buf is there
+autocmd({ "UIEnter", "BufReadPost", "BufNewFile" }, {
+  group = vim.api.nvim_create_augroup("joonasrissanen/FilePost", { clear = true }),
+  callback = function(args)
+    local file = vim.api.nvim_buf_get_name(args.buf)
+    local buftype = vim.api.nvim_get_option_value("buftype", { buf = args.buf })
+
+    if not vim.g.ui_entered and args.event == "UIEnter" then
+      vim.g.ui_entered = true
+    end
+
+    if file ~= "" and buftype ~= "nofile" and vim.g.ui_entered then
+      vim.api.nvim_exec_autocmds("User", { pattern = "FilePost", modeline = false })
+      vim.api.nvim_del_augroup_by_name "joonasrissanen/FilePost"
+
+      vim.schedule(function()
+        vim.api.nvim_exec_autocmds("FileType", {})
+
+        if vim.g.editorconfig then
+          require("editorconfig").config(args.buf)
+        end
+      end)
+    end
+  end,
+})
+
 -- apply dotenv changes automatically on save
 autocmd("BufWritePost", {
   pattern = vim.fn.expand "~" .. "/.local/share/chezmoi/*",
   command = 'silent ! chezmoi apply --source-path "%"',
-})
-
-autocmd("BufWritePre", {
-  pattern = "*.go",
-  callback = function()
-    local params = vim.lsp.util.make_range_params()
-    params.context = { only = { "source.organizeImports" } }
-    -- buf_request_sync defaults to a 1000ms timeout. Depending on your
-    -- machine and codebase, you may want longer. Add an additional
-    -- argument after params if you find that you have to write the file
-    -- twice for changes to be saved.
-    -- E.g., vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 3000)
-    local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params)
-    for cid, res in pairs(result or {}) do
-      for _, r in pairs(res.result or {}) do
-        if r.edit then
-          local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
-          vim.lsp.util.apply_workspace_edit(r.edit, enc)
-        end
-      end
-    end
-    vim.lsp.buf.format { async = false }
-  end,
 })
