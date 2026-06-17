be0185a6adf77ad2dd141b9029b947522ddd00a3
Joonas Rissanen <rissanen.s.joonas@gmail.com>
2026-06-17 12:01:24 +0300

    remove lazygit neovim plugin

--- dot_config/nvim/lua/plugins/git.lua
+++ dot_config/nvim/lua/plugins/git.lua
@@ -39,25 +39,4 @@ return {
       require("gitsigns").setup(opts)
     end,
   },
-  {
-    "kdheepak/lazygit.nvim",
-    lazy = false,
-    cmd = {
-      "LazyGit",
-      "LazyGitConfig",
-      "LazyGitCurrentFile",
-      "LazyGitFilter",
-      "LazyGitFilterCurrentFile",
-    },
-    requires = {
-      "nvim-telescope/telescope.nvim",
-      "nvim-lua/plenary.nvim",
-    },
-    keys = {
-      { "<leader>gg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
-    },
-    config = function()
-      require("telescope").load_extension "lazygit"
-    end,
-  },
 }
